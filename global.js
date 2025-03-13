dc3eac639e185b8400fb650a55f92b6df57f9779
Marcelo Angelo <masfz@Marcelos-MacBook-Pro.local>
2025-03-13 12:43:28 -0300

    adicionando js

---
+++ global.js
@@ -0,0 +1,39 @@
+console.log("IT'S ALIVE!!!")
+
+
+let pages = [
+    {url: "/", title: "Home"},
+    {url: "/projects/projects.html", title: "Projects"},
+    {url: "/contact/contact.html", title: "Contact"},
+    {url: "/cv/cv.html", title: "cv"},
+]
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+let ul = document.createElement("ul");
+nav.appendChild(ul);
+nav.classList.add("nav-menu")
+
+for (let p of pages) {
+    let a = document.createElement("a");
+    let li = document.createElement("li");
+    li.appendChild(a)
+    a.href=p.url;
+    a.innerText=p.title;
+    ul.appendChild(li);
+}
+
+function $$ (selector, context = document){
+    return Array.from(context.querySelectorAll(selector));
+}
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+
+currentLink.classList.add("current");
+
+if (currentLink) {
+    currentLink.classList.add("current");
+}
